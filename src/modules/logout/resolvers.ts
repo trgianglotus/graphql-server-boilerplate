e78c0b97f05a8d4379333d9fc129c2f9b6d5486a
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-11-01 21:39:58 +0800

    Lock User Account GraphQL

--- src/modules/logout/resolvers.ts
+++ src/modules/logout/resolvers.ts
@@ -1,5 +1,6 @@
 import { userSessionIdPrefix, redisSessionPrefix } from '../../constants';
 import { ResolverMap } from '../../types/graphql-utils';
+import { removeAllUsersSessions } from '../../utils/removeAllUsersSessions';
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -19,17 +20,7 @@ export const resolvers: ResolverMap = {
       const { userId } = session;
 
       if (userId) {
-        const sessionIds = await redis.lrange(
-          `${userSessionIdPrefix}${userId}`,
-          0,
-          -1
-        );
-
-        const promises = [];
-        for (let i = 0; i < sessionIds.length; i += 1) {
-          promises.push(redis.del(`${redisSessionPrefix}${sessionIds[i]}`));
-        }
-        await Promise.all(promises);
+        removeAllUsersSessions(userId, redis);
         return true;
       }
 
